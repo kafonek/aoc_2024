c9330614a8ca6b35451f99aed252d6e99b870f99
Matt Kafonek <mkafonek@runwayml.com>
2024-12-01 16:19:21 +0000

    working out benchmarking

--- rust/benches/timing.rs
+++ rust/benches/timing.rs
@@ -2,8 +2,8 @@ use aoc_2024::day01;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("day01 part1", |b| b.iter(|| day01::part1("data/01.txt")));
-    c.bench_function("day01 part2", |b| b.iter(|| day01::part2("data/01.txt")));
+    c.bench_function("day01 part1", |b| b.iter(|| day01::part1("01.txt")));
+    c.bench_function("day01 part2", |b| b.iter(|| day01::part2("01.txt")));
 }
 
 criterion_group!(benches, criterion_benchmark);
