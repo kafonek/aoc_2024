957569b4de6e4b461a85a8133584c58d3bc4b36c
Matt Kafonek <mkafonek@runwayml.com>
2024-12-02 16:27:16 +0000

    day2

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,9 +1,13 @@
 use aoc_2024::day01::Day01;
+use aoc_2024::day02::Day02;
 use aoc_2024::utils::FromLines;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct WasmDay01(Day01);
+
+#[wasm_bindgen]
+pub struct WasmDay02(Day02);
 
 #[wasm_bindgen]
 impl WasmDay01 {
@@ -21,5 +25,24 @@ impl WasmDay01 {
     #[wasm_bindgen]
     pub fn similarity(&self) -> i32 {
         self.0.similarity()
+    }
+}
+
+#[wasm_bindgen]
+impl WasmDay02 {
+    #[wasm_bindgen]
+    pub fn from_content(content: &str) -> WasmDay02 {
+        let data = Day02::from_lines(content.lines().map(String::from));
+        WasmDay02(data)
+    }
+
+    #[wasm_bindgen]
+    pub fn count_safe_reports(&self) -> i32 {
+        self.0.count_safe_reports()
+    }
+
+    #[wasm_bindgen]
+    pub fn count_safe_reports_with_dampener(&self) -> i32 {
+        self.0.count_safe_reports_with_dampener()
     }
 }
