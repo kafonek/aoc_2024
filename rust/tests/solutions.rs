3d54c79105db1b9a27b9ffe08a57036ce4138050
Matt Kafonek <mkafonek@runwayml.com>
2024-12-02 15:27:06 +0000

    day2 regression tests

--- rust/tests/solutions.rs
+++ rust/tests/solutions.rs
@@ -1,9 +1,17 @@
 use aoc_2024::day01;
-
+use aoc_2024::day02;
 #[test]
 fn test_day01() {
     assert_eq!(day01::part1("01_sample.txt"), 11);
     assert_eq!(day01::part1("01.txt"), 2344935);
     assert_eq!(day01::part2("01_sample.txt"), 31);
     assert_eq!(day01::part2("01.txt"), 27647262);
+}
+
+#[test]
+fn test_day02() {
+    assert_eq!(day02::part1("02_sample.txt"), 2);
+    assert_eq!(day02::part1("02.txt"), 282);
+    assert_eq!(day02::part2("02_sample.txt"), 4);
+    assert_eq!(day02::part2("02.txt"), 349);
 }
